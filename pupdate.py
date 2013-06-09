4ff881851751a9153dfcedd849411c0b7316e143
Daniel Kratzert <dkratzert@gmx.de>
2013-06-09 21:23:52 +0200

    more comments and cleanup

--- pupdate.py
+++ pupdate.py
@@ -3,36 +3,28 @@
 #möp
 # this script updates the platon.exe in the c:\pwt directory
 
-#http://www.cryst.chem.uu.nl/spek/xraysoft/mswindows/platon/platon.zip
-
 import urllib2
-import time
 import os
 import tempfile
 import zipfile
 
-timestr = time.strftime("%Y%m%d")
-
 u = urllib2.urlopen('http://www.cryst.chem.uu.nl/spek/xraysoft/mswindows/platon/platon.zip')
 
-
-#File = 'platon-%s.zip' %timestr
-#print '%20s  %s' % (File, zipfile.is_zipfile(File))
-
+#create a temporary file and download platon to it.
 localFile = tempfile.TemporaryFile()
 localFile.write(u.read())
-#localFile.close()
 
+#open the zip file
 zf = zipfile.ZipFile(localFile, 'r')
 
-list = zf.namelist()  #list of files in the zipfile
-#print list
+#list of files in the zipfile
+list = zf.namelist()  
 
+# extract the files to the platon directory
 for name in list:
 	fd = open('c:/pwt/'+name, "wb")
-	#print fd
 	fd.write(zf.read(name))
 	print 'extracting', name
 	fd.close()
 
-#os.remove(File)
+localFile.close()
