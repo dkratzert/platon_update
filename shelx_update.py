26964984a3f1e875d705fd687a7828f06aabe1f4
Daniel Kratzert <dkratzert@gmx.de>
2013-06-09 11:31:43 +0200

    progress bar

--- shelx_update.py
+++ shelx_update.py
@@ -17,7 +17,41 @@ timestr = time.strftime("%Y%m%d")
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
+filename = "install_shelx_win64.exe"
 fileurl = 'http://shelx.uni-ac.gwdg.de/~gsheldr/bin/install_shelx_win64.exe'
+#fileurl = 'http://shelx.uni-ac.gwdg.de/~gsheldr/bin/linux/shelxl.bz2'
+
+
+def chunk_report(bytes_so_far, chunk_size, total_size):
+   percent = float(bytes_so_far) / total_size
+   percent = round(percent*100, 2)
+   sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" % 
+       (bytes_so_far, total_size, percent))
+
+   if bytes_so_far >= total_size:
+      sys.stdout.write('\n')
+
+def chunk_read(response, chunk_size=8192, report_hook=None):
+   total_size = response.info().getheader('Content-Length').strip()
+   total_size = int(total_size)
+   bytes_so_far = 0
+
+   while 1:
+      chunk = response.read(chunk_size)
+      bytes_so_far += len(chunk)
+
+      if not chunk:
+         break
+
+      if report_hook:
+         report_hook(bytes_so_far, chunk_size, total_size)
+
+   return bytes_so_far
+
+
+   response = urllib2.urlopen('http://www.ebay.com');
+   chunk_read(response, report_hook=chunk_report)
+
 
 # create a password manager
 password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
@@ -39,7 +73,17 @@ opener.open(fileurl)
 # Now all calls to urllib2.urlopen use our opener.
 urllib2.install_opener(opener)
 
+#print "test"
 
+if __name__ == '__main__':
+    req = urllib2.Request(fileurl)
+    response = urllib2.urlopen(req)
+    #the_file = response.read()
+    the_file = chunk_read(response, report_hook=chunk_report)
 
-localFile = tempfile.TemporaryFile()
-localFile.write(opener.read())
+    # Just create a temporary file and dont keep it
+    #localFile = tempfile.TemporaryFile()
+    #localFile.write(the_file)
+
+    localFile = open(filename, 'wb')
+    localFile.write(the_file)
