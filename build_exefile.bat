590f708402e55ca9af1a3bc869f529dc14b99c4e
Daniel Kratzert <dkratzert@gmx.de>
2013-06-14 20:43:20 +0200

    build script added

---
+++ build_exefile.bat
@@ -0,0 +1,16 @@
+
+rem C:\Users\daniel\Downloads\pupdate_exes>
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py shelxle_update.py
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py -F --onefile shelxle_update.py shelxle_update.spec 
+
+
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py shelx_update.py
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py -F --onefile shelx_update.py shelx_update.spec 
+
+
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py pupdate.py
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py -F --onefile pupdate.py pupdate.spec 
+
+
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py xt_update.py
+python c:\Users\daniel\Downloads\pyinstaller-2.0\pyinstaller.py -F --onefile xt_update.py xt_update.spec 
