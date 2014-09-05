5b36a3165f997cf8c2f830b65b35e818f2a37090
Daniel Kratzert <dkratzert@gmx.de>
2014-09-05 16:08:38 +0200

    translation

--- viewframe.bat
+++ viewframe.bat
@@ -1,4 +1,5 @@
-rem Dieses skript ermöglich das betrachten von Bruker Frames
+rem This script allows to view Bruker frames in Windows without starting APEX2
+rem Dieses skript ermoeglich das betrachten von Bruker Frames
 
 C:\bn\Tools\frm2frm.exe %1 /F:5 /OUT:%TMP%\view.jpg /A /B /T
 
