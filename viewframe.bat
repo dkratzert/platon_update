7e8f318a6d9aec5883ec4663a81ccd076be650b1
Daniel Kratzert <dkratzert@gmx.de>
2015-09-03 08:57:20 +0200

    more description

--- viewframe.bat
+++ viewframe.bat
@@ -1,9 +1,9 @@
-rem This script allows to view Bruker frames in Windows without starting APEX2
-rem Dieses skript ermoeglich das betrachten von Bruker Frames
+REM This script allows to view Bruker frames in Windows without starting APEX2
+REM Place it in e.g. c:\program files\ and associate it with the .sfrm file ending. 
+REM Now you can view frames in a fraction of a second.
 
 C:\bn\Tools\frm2frm.exe %1 /F:5 /OUT:%TMP%\view.jpg /A /B /T
-
-rem D:\programme\IrfanView\i_view32.exe %TMP%\view.jpg 
-
 mspaint.exe %TMP%\view.jpg
 
+
+rem D:\programme\IrfanView\i_view32.exe %TMP%\view.jpg 
