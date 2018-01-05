e5efda41ff234a84998cbbd4e299893e7e5034dc
Daniel Kratzert <dkratzert@gmx.de>
2018-01-05 10:20:16 +0100

    ported to Python3

--- pupdate.py
+++ pupdate.py
@@ -1,26 +1,43 @@
 #/usr/bin/env python
 #-*- encoding: utf-8 -*-
 #möp
-# this script updates the platon.exe in the c:\pwt directory
-
-import urllib2
+# This script updates the platon.exe in the c:\pwt directory.
+pyver = 2
+try:
+    import urllib2
+except ModuleNotFoundError:
+    import urllib.request
+    pyver = 3
 import os
 import sys
 import time
 import tempfile
 import zipfile
-from HTMLParser import HTMLParser
+if pyver == 2:
+    from HTMLParser import HTMLParser
+if pyver == 3:
+    from html.parser import HTMLParser
 import datetime
-
+changes_url = 'http://www.platonsoft.nl/spek/xraysoft/update_history_platon.html'
+platon_url = 'http://www.platonsoft.nl/spek/xraysoft/mswindows/platon/platon.zip'
 
 # fetch the changes of platon and store it in one string (text)
-html = urllib2.urlopen('http://www.platonsoft.nl/spek/xraysoft/update_history_platon.html')
-text = [html.next() for x in range(18)]
-text = "".join(text)
+if pyver == 2:
+    htmlp = urllib2.urlopen(changes_url)
+    text = [htmlp.next() for x in range(25)]
+    text = "".join(text)
+if pyver == 3:
+    with urllib.request.urlopen(changes_url) as f:
+        text = [x.decode('utf-8') for x in f.readlines()]
+    text = "".join(text[:25])
+else:
+    print("Unable to download platon. Use Python2 or 3.")
+
 
 #strips down the html to plain text
 class MLStripper(HTMLParser):
     def __init__(self):
+        super().__init__()
         self.reset()
         self.fed = []
     def handle_data(self, d):
@@ -28,22 +45,28 @@ class MLStripper(HTMLParser):
     def get_data(self):
         return ''.join(self.fed)
 
-def strip_tags(html):
+def strip_tags(htmlp):
     s = MLStripper()
-    s.feed(html)
+    s.feed(htmlp)
     return s.get_data()
 
 today = datetime.date.today()
-print "\nToday is: " +str(today.strftime('%b %d, %Y\n'))
-print strip_tags(text)    
+print("\nToday is: {}".format(str(today.strftime('%b %d, %Y\n'))))
+print(strip_tags(text))
     
 
 # now preceed with platon update
-u = urllib2.urlopen('http://www.platonsoft.nl/spek/xraysoft/mswindows/platon/platon.zip')
+if pyver == 2:
+    u = urllib2.urlopen(platon_url)
+    data = u.read()
+if pyver == 3:
+    req = urllib.request.Request(platon_url)
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
 
 #create a temporary file and download platon to it.
 localFile = tempfile.TemporaryFile()
-localFile.write(u.read())
+localFile.write(data)
 
 #open the zip file
 zf = zipfile.ZipFile(localFile, 'r')
@@ -57,17 +80,17 @@ if not dir:
     try:
         os.mkdir('c:\\pwt')
     except IOError:
-        print "unable to create install directory"
+        print("unable to create install directory")
         sys.exit()
     else:
-        print 'created platon directory c:\\pwt'
+        print('created platon directory c:\\pwt')
         pass
 # extract the files to the platon directory
 for name in list:
     fd = open('c:/pwt/'+name, "wb")
     fd.write(zf.read(name))
-    print 'extracting', name
+    print('extracting {}'.format(name))
     fd.close()
 
 localFile.close()
-time.sleep(8)
+time.sleep(10)
